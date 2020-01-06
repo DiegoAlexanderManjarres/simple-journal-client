ecacb1cedbfde285d75caa53fb761c0314bf20bd
Diego Alexander Manjarres <renmasuo@debian-buster.local>
2020-01-06 10:41:11 -0500

    loading on dashboard 3

--- src/components/pages/common/loading.js
+++ src/components/pages/common/loading.js
@@ -2,17 +2,17 @@ import React from "react"
 import '../../../styles/loading/base.scss'
 
 
-const LoadingPage = () => (
+const LoadingPage = props => (
   <section>
       
     <header>
-    <div className='circle'></div>
+        <div className='circle'></div>
         <h1 className='change' style={{ textAlign: 'center' }}>LOADING</h1>
     </header> 
-    <div id='content'>
+    
+    <div id='content' >
         <span className='expand'></span>
-    </div>
-      
+    </div>      
       
   </section>
 )
