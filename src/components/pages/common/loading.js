c9eebe82c5812ca66c38eb7ca2e04bb19da9c05b
Diego Alexander Manjarres <renmasuo@debian-buster.local>
2020-01-11 00:32:36 -0500

    loading on dashboard 19

--- src/components/pages/common/loading.js
+++ src/components/pages/common/loading.js
@@ -7,12 +7,10 @@ import loadingThemes from '../../../styles/loading/loadingTheme'
 const LoadingPage = ({ _className, ...rest }) => {
     const { name } = useContext(ThemeContext)
     const theme = loadingThemes[name]
-    
+    console.log(name, ': ', theme[_className])
+    const _classname = _className ? theme[_className] : null
     return (
-        <section className={() => {
-            console.log(name, ': ', theme[_className])
-            return _className ? theme[_className] : null
-            }}>            
+        <section className={_classname}>            
             <header>
                 <div className={theme.circle}></div>
                 <h1 className={theme.change}>LOADING</h1>
